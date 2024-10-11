f08361c5ed5beb6f9111dfbe175b0e8f794e45cb
ADRIEN D <ad.villermois@gmail.com>
2024-10-11 21:00:09 +0200

    *

--- app/models/category.ts
+++ app/models/category.ts
@@ -1,13 +1,20 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Deck from './deck.js'
+import { type HasMany } from '@adonisjs/lucid/types/relations'
 
 export default class Category extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  @column()
+  declare name: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  @column()
+  declare iconName: string
+
+  @column()
+  declare iconLibrary: string
+
+  @hasMany(() => Deck)
+  declare decks: HasMany<typeof Deck>
 }
